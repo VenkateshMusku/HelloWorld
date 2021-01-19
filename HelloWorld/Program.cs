2d6300501dea84c16232a7670af356afd180987d
Venkatesh Musku <60488590+VenkateshMusku@users.noreply.github.com>
2021-01-19 15:20:13 -0500

    Version 2 of HelloWorld!

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,12 +1,15 @@
-﻿using System;
+﻿using System;//
 
 namespace HelloWorld
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) //constructor method
         {
+            //Pause to display HelloWorld!
+            Console.WriteLine("Press any key to continue...");
             Console.WriteLine("Hello World!");
+            Console.ReadKey(true);
         }
     }
 }
